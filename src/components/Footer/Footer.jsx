6cc1b521bb9a47c850e5b9bd90c45476bb726cd6
4digitechIntl <digitech2079@gmail.com>
2023-04-09 14:35:53 +0545

    first one

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
-// import revert from "../../assests/images/revert-logo.png";
 import revert from "../../assests/images/whitebglogo.jpg";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
