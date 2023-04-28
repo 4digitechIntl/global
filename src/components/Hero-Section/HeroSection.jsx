bf038a2d1a388ad43585a961ca5f50cb091277df
4digitechIntl <digitech2079@gmail.com>
2023-04-28 16:55:08 +0545

    first one

--- src/components/Hero-Section/HeroSection.jsx
+++ src/components/Hero-Section/HeroSection.jsx
@@ -4,6 +4,10 @@ import "./hero-section.css";
 import Carousel from 'react-bootstrap/Carousel';
 import japan from "../../assests/images/japan.jpg";
 import banner1 from "../../assests/images/banner1.jpeg";
+import banner2 from "../../assests/images/banner2.jpeg";
+import banner3 from "../../assests/images/banner3.jpeg";
+import banner4 from "../../assests/images/banner4.jpeg";
+import banner5 from "../../assests/images/banner5.jpeg";
 import stdone from "../../assests/images/stdone.png";
 import usa from "../../assests/images/usa.jpg";
 import australia from "../../assests/images/australia.jpg";
@@ -15,13 +19,13 @@ const HeroSection = () => {
     <section>
         <Carousel>
           <Carousel.Item>
-            <span className="overlay"/>
+            {/* <span className="overlay"/> */}
             <img
               className="d-block w-100 slider-img"
-              src={carousel_one}
+              src={banner1}
               alt="Second slide"
             />
-              <Row className="hero-banner">
+              {/* <Row className="hero-banner">
                 <Col classname="col-img">
                   <img className="hero-holder" src={stdone} alt="student" />
                 </Col>
@@ -35,60 +39,41 @@ const HeroSection = () => {
                   <span>Contact Us</span>
                 </a>
                 </Col>
-              </Row>
+              </Row> */}
           </Carousel.Item>
-          
           <Carousel.Item>
-            <span className="overlay"/>
+            {/* <span className="overlay"/> */}
             <img
               className="d-block w-100 slider-img"
-              src={banner1}
+              src={banner2}
               alt="Second slide"
             />
-            <Container>
-              <Row className="hero-banner">
-                <Col classname="col-img">
-                  <img className="hero-holder" src={stdone} alt="student" />
-                </Col>
-                <Col className="my-auto px-5 col-text ">
-                  <h3 className="hero-header">The future you imagine is closer than you think.</h3>
-                  <p className="hero-sub">Our mere ambition lies together with your success. Out entire team is dedicatedly
-                    committed to serve the best with our nitty-gritty services and regular conversation with
-                    you.
-                  </p>
-                  <a className="btn-hero" href='/#contact'>
-                    <span>Contact Us</span>
-                  </a>
-                </Col>
-              </Row>
-            </Container>
           </Carousel.Item>
-
           <Carousel.Item>
-            <span className="overlay"/>
+            {/* <span className="overlay"/> */}
             <img
               className="d-block w-100 slider-img"
-              src={usa}
+              src={banner3}
               alt="Second slide"
             />
-            <Container>
-              <Row className="hero-banner">
-                <Col classname="col-img">
-                  <img className="hero-holder" src={stdone} alt="student" />
-                </Col>
-                <Col className="my-auto px-5 col-text ">
-                  <h3 className="hero-header">The future you imagine is closer than you think.</h3>
-                  <p className="hero-sub">Our mere ambition lies together with your success. Out entire team is dedicatedly
-                    committed to serve the best with our nitty-gritty services and regular conversation with
-                    you.
-                  </p>
-                <a className="btn-hero" href='/#contact'>
-                  <span>Contact Us</span>
-                </a>
-                </Col>
-              </Row>
-            </Container>
           </Carousel.Item>
+          <Carousel.Item>
+            {/* <span className="overlay"/> */}
+            <img
+              className="d-block w-100 slider-img"
+              src={banner4}
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            {/* <span className="overlay"/> */}
+            <img
+              className="d-block w-100 slider-img"
+              src={banner5}
+              alt="Second slide"
+            />
+          </Carousel.Item>
+        
         </Carousel>
     </section>
   );
